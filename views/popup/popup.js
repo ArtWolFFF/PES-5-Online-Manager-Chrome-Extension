c9a74dde68016160353245945aa844f657a5ced3
avolkov <artwolfff@gmail.com>
2019-05-26 18:27:14 +0300

    Layout blueprints (wip)

---
+++ views/popup/popup.js
@@ -0,0 +1,12 @@
+let league = {
+    SerieA: "seriea.l98",
+    Ligue1: "ligue1.l98",
+    LaLiga: "example.l98",
+    EuroLeague: "euroleague.l98",
+    EPL: "epl.l98",
+    Bundesliga: "bundesliga.l98"
+};
+
+let links = {
+
+};
