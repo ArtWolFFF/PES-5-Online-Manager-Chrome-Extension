65d49dfede696d401a479dfbb05cb77f41918ea0
avolkov <artwolfff@gmail.com>
2019-05-31 23:10:01 +0300

    Cleaning up

--- scripts/background/backgroundPsd.js
+++
@@ -1,8 +0,0 @@
-  chrome.webNavigation.onCompleted.addListener(handlePsdVisit,
-	{url: [{hostEquals : 'pesstatsdatabase.com', pathSuffix: 'Player_old2011.php'}]}
-  );
-  
-  
-  function handlePsdVisit() {
-	  //
-  }
